9e2d5b5c5c7c307030896bc4c87afa679a21a128
JiXu Luo <goodluck1031@outlook.com>
2021-08-22 13:55:20 +0900

    update

--- vhost.manager/_compile/content/game/bet_list.html.php
+++ vhost.manager/_compile/content/game/bet_list.html.php
@@ -376,7 +376,7 @@
 			</tr>
 			
 			<tr id="d_<?php echo $TPL_V1["betting_no"]?>" <?php if($TPL_VAR["show_detail"]==0){?>style="display:none;"<?php }?> class="gameDetail">
-				<td colspan="15">
+				<td colspan="16">
 					<table cellspacing="1" id="d_<?php echo $TPL_V1["betting_no"]?>">
 						<tr>				  
 							<th>게임타입</th>
@@ -388,7 +388,8 @@
 							<th>배팅방향</th>
 							<th>배팅배당</th>
 							<th>총 배팅액</th>
-							<th>스코어</th>
+							<th>배팅스코어</th>
+							<th>최종스코어</th>
 							<th>결과</th>
 							<th>상태</th>
 							<th>처리</th>
@@ -558,13 +559,9 @@
 								<td width="40" align="center" style="border-bottom:1px #CCCCCC solid;color: #666666"><?php echo $TPL_V2["select_rate"];?></td>
 								<td width="20" align="" style="border-bottom:1px #CCCCCC solid;color: #666666"><a href="javascript:open_window('/member/popup_bet_detail?idx=<?php echo $TPL_V2["sub_child_sn"]?>',600,400)"><?=number_format($TPL_V2["nTotalBetMoney"],0)?></a></td>
 								<td width="40" align="center" style="border-bottom:1px #CCCCCC solid;color: #666666" id="<?php echo $TPL_V1["betting_no"].'_'.$TPL_V2["total_betting_sn"]?>_score">
-								<?php 
-									if($TPL_V2["live"] == 1)
-										echo $TPL_V2["score"];
-									else 
-										echo $TPL_V2["home_score"] . "-" . $TPL_V2["away_score"];
-								?>
+								<?=$TPL_V2["score"]?>
 								</td>
+								<td width="40"><?=$TPL_V2["home_score"] . "-" . $TPL_V2["away_score"]?></td>
 								<td width="50" align="center" style="border-bottom:1px #CCCCCC solid;color: #666666" id="<?php echo $TPL_V1["betting_no"].'_'.$TPL_V2["total_betting_sn"]?>_result">
 							<?php if($TPL_V2["win"]==1){?>[홈승]
 							<?php }elseif($TPL_V2["win"]==3){?>[무승부]
