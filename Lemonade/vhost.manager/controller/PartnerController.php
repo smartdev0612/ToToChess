ef33a0e40df8b35faa1d26407701dcd586b6726b
JiXu Luo <goodluck1031@outlook.com>
2021-09-04 20:20:15 +0900

    update

--- Lemonade/vhost.manager/controller/PartnerController.php
+++ Lemonade/vhost.manager/controller/PartnerController.php
@@ -961,6 +961,7 @@ class PartnerController extends WebServiceController
 		
 		$act = $this->request('act');
 		$p_type	= empty($this->request('p_type')) ? 0 : $this->request('p_type');
+		$perpage = empty($this->request("perpage")) ? 10 : $this->request("perpage");
 
 		if(isset($act) && $act == "del")
 		{
@@ -981,9 +982,9 @@ class PartnerController extends WebServiceController
 		
 		$where = "and fromid = '운영팀' and kubun='1' and p_type = '" . $p_type . "'";
 
-		$page_act = "p_type=".$p_type;
+		$page_act = "perpage=" . $perpage . "&p_type=".$p_type;
 		$total = $mModel->getMemoTotal($where);
-		$pageMaker = $this->displayPage($total, 10, $page_act);
+		$pageMaker = $this->displayPage($total, $perpage, $page_act);
 		$list = $mModel->getMemoList($where, $pageMaker->first, $pageMaker->listNum, " writeday desc ");
 		
 		$this->view->assign('list', $list);
