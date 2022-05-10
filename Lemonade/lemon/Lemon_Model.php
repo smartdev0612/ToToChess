ccd0a0c5c487aea95cb21e61080bef5017c81744
Jimmy <goodluck1031@outlook.com>
2022-05-10 23:14:41 +0900

    update opensl

--- Lemonade/lemon/Lemon_Model.php
+++ Lemonade/lemon/Lemon_Model.php
@@ -106,14 +106,14 @@ class Lemon_Model extends Lemon_Object {
 	// 문자렬 암호화
 	function Encrypt($str, $secret_key='secret key', $secret_iv='secret iv') { 
 		$key = hash('sha256', $secret_key); 
-		$iv = substr(hash('sha256', $secret_iv), 0, 32); 
+		$iv = substr(hash('sha256', $secret_iv), 0, 16); 
 		return str_replace("=", "", base64_encode( openssl_encrypt($str, "AES-256-CBC", $key, 0, $iv)) ); 
 	} 
 	
 	// 문자렬 복호화
 	function Decrypt($str, $secret_key='secret key', $secret_iv='secret iv') { 
 		$key = hash('sha256', $secret_key); 
-		$iv = substr(hash('sha256', $secret_iv), 0, 32); 
+		$iv = substr(hash('sha256', $secret_iv), 0, 16); 
 		return openssl_decrypt( base64_decode($str), "AES-256-CBC", $key, 0, $iv ); 
 	}
 
